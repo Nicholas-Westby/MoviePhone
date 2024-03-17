f48d97f3694773458f544156f9753b8fecf983be
nicholas_westby <w@minify.org>
2024-03-16 19:37:27 -0700

    feat (Text) center text and make it larger

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React, {Component, ReactNode} from "react";
-import {SafeAreaView, Text} from "react-native";
+import {SafeAreaView, Text, StyleSheet} from "react-native";
 
 interface AppProps {
 
@@ -12,11 +12,22 @@ interface AppState {
 class App extends Component<AppProps, AppState> {
   render(): ReactNode {
     return (
-      <SafeAreaView>
-        <Text>Hi</Text>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.header}>Hi</Text>
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 40,
+  },
+});
 
 export default App;
